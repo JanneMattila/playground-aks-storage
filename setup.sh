bd34c8fb6325a89785fa47de7ad26b84bc6c41ea
Janne Mattila <janne.mattila@hotmail.fi>
2021-11-26 10:30:22 +0200

    Update demo

--- setup.sh
+++ setup.sh
@@ -60,6 +60,10 @@ echo $identityid
 
 az aks get-versions -l $location -o table
 
+# Note: for public cluster you need to authorize your ip to use api
+myip=$(curl --no-progress-meter https://api.ipify.org)
+echo $myip
+
 # Note about private clusters:
 # https://docs.microsoft.com/en-us/azure/aks/private-clusters
 
@@ -81,20 +85,15 @@ az aks create -g $resourceGroupName -n $aksName \
  --load-balancer-sku standard \
  --vnet-subnet-id $subnetaksid \
  --assign-identity $identityid \
+ --api-server-authorized-ip-ranges $myip \
  -o table 
 
 ###################################################################
-# Note: for public cluster you need to authorize your ip to use api
-myip=$(curl --no-progress-meter https://api.ipify.org)
-echo $myip
-
 # Enable current ip
-az aks update -g $resourceGroupName -n $aksName \
-  --api-server-authorized-ip-ranges $myip
+az aks update -g $resourceGroupName -n $aksName --api-server-authorized-ip-ranges $myip
 
 # Clear all authorized ip ranges
-az aks update -g $resourceGroupName -n $aksName \
-  --api-server-authorized-ip-ranges ""
+az aks update -g $resourceGroupName -n $aksName --api-server-authorized-ip-ranges ""
 ###################################################################
 
 sudo az aks install-cli
@@ -132,20 +131,28 @@ kubectl apply -f azurefile-csi-nfs/azurefile-csi-nfs.yaml
 # ...
 # azurefile-csi-nfs       file.csi.azure.com         Delete          Immediate              true                   3s
 
-# Create Premium ZRS file share
-az storage account create \
+# Create Premium ZRS file share ahead of time => static provisioning
+# NFS related notes from Azure portal if "Secure transfer required" is enabled:
+#   "Secure transfer required" is a setting that is enabled for this storage account. 
+#   The NFS protocol does not support encryption and relies on network-level security. 
+#   This setting must be disabled for NFS to work.
+storageid=$(az storage account create \
   --name $premiumStorageName \
   --resource-group $resourceGroupName \
   --location $location \
   --sku Premium_ZRS \
   --kind FileStorage \
-  --https-only
+  --allow-blob-public-access false \
+  --public-network-access Disabled \
+  --https-only false \
+  --query id -o tsv)
+echo $storageid
 
 premiumStorageKey=$(az storage account keys list \
   --account-name $premiumStorageName \
   --resource-group $resourceGroupName \
   --query [0].value \
-  -o TSV)
+  -o tsv)
 echo $premiumStorageKey
 
 az storage share-rm create --access-tier Premium --enabled-protocols SMB --quota 100 --name $premiumStorageShareNameSMB --storage-account $premiumStorageName
@@ -157,6 +164,68 @@ az storage share-rm create --access-tier Premium --enabled-protocols NFS --quota
 # Maximum IO/s     500
 # Burst IO/s       4000
 # Throughput rate  70.0 MiBytes / s
+
+# Follow instructions from here:
+# https://docs.microsoft.com/en-us/azure/storage/files/storage-files-networking-endpoints?tabs=azure-cli
+# Disable private endpoint network policies
+az network vnet subnet update \
+  --ids $subnetstorageid \
+  --disable-private-endpoint-network-policies \
+  --output none
+
+# Create private endpoint to "StorageSubnet"
+storagepeid=$(az network private-endpoint create \
+    --name storage-pe \
+    --resource-group $resourceGroupName \
+    --vnet-name $vnetName --subnet $subnetStorage \
+    --private-connection-resource-id $storageid \
+    --group-id file \
+    --connection-name storage-connection \
+    --query id -o tsv)
+echo $storagepeid
+
+# Create Private DNS Zone
+fileprivatednszoneid=$(az network private-dns zone create \
+    --resource-group $resourceGroupName \
+    --name "privatelink.file.core.windows.net" \
+    --query id -o tsv)
+echo $fileprivatednszoneid
+
+# Link Private DNS Zone to VNET
+az network private-dns link vnet create \
+  --resource-group $resourceGroupName \
+  --zone-name "privatelink.file.core.windows.net" \
+  --name file-dnszone-link \
+  --virtual-network $vnetName \
+  --registration-enabled false
+
+# Get private endpoint NIC
+penicid=$(az network private-endpoint show \
+  --ids $storagepeid \
+  --query "networkInterfaces[0].id" -o tsv)
+echo $penicid
+
+# Get ip of private endpoint NIC
+peip=$(az network nic show \
+  --ids $penicid \
+  --query "ipConfigurations[0].privateIpAddress" -o tsv)
+echo $peip
+
+# Map private endpoint ip to A record in Private DNS Zone
+az network private-dns record-set a create \
+  --resource-group $resourceGroupName \
+  --zone-name "privatelink.file.core.windows.net" \
+  --name $premiumStorageName \
+  --output none
+
+az network private-dns record-set a add-record \
+  --resource-group $resourceGroupName \
+  --zone-name "privatelink.file.core.windows.net" \
+  --record-set-name $premiumStorageName \
+  --ipv4-address $peip \
+  --output none
+
+# Start kubernetes app deployments
 
 kubectl apply -f namespace.yaml
 
@@ -211,8 +280,23 @@ cat <<EOF > payload.json
 }
 EOF
 
+# Quick tests
+# - NFS
 curl --no-progress-meter -X POST --data '{"path": "/mnt/nfs","filter": "*.*","recursive": true}' -H "Content-Type: application/json" "http://$ingressip/api/files" | jq .milliseconds
+# - SMB
 curl --no-progress-meter -X POST --data '{"path": "/mnt/smb","filter": "*.*","recursive": true}' -H "Content-Type: application/json" "http://$ingressip/api/files" | jq .milliseconds
+
+# Test same in loop
+# - NFS
+for i in {0..50}
+do 
+  curl --no-progress-meter -X POST --data '{"path": "/mnt/nfs","filter": "*.*","recursive": true}' -H "Content-Type: application/json" "http://$ingressip/api/files" | jq .milliseconds
+done
+# - SMB
+for i in {0..50}
+do 
+  curl --no-progress-meter -X POST --data '{"path": "/mnt/smb","filter": "*.*","recursive": true}' -H "Content-Type: application/json" "http://$ingressip/api/files" | jq .milliseconds
+done
 
 # Connect to first pod
 pod1=$(kubectl get pod -n demos -o name | head -n 1)
@@ -246,6 +330,7 @@ ls perf-test/*.0
 # Remove test files
 rm perf-test/*.0
 
+# Exit container shell
 exit
 
 # Wipe out the resources
